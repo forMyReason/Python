744ec3abcd3c19917ee0bbc1950497b11c093f59
LiDanyang <791602449@qq.com>
2024-08-21 17:35:35 +0800

    base_create_folder(1/10)

---
+++ make_dir.py
@@ -0,0 +1,8 @@
+import os
+
+path = r'D:/ComacMetaUniverse_5.4/comacmetauniverse/Content/Arts/StaticMesh/HJ_HuanJing'
+B_path = [06,09,10,16,17,18,22,24,26,50,51,54,55,56] 
+C_path = [01,02,03,11,12,13,51,52,54,55,57,59] 
+D_path = [01,02,04,05,06,08_09]
+if not os.path.exists(newpath):
+    os.makedirs(newpath)
