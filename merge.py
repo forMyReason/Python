8f8e40436e8dd8da9f9345aefee255c67b5cbea8
LiDanyang <791602449@qq.com>
2024-08-23 15:14:35 +0800

    跑通了流程了，但是大量数据的时候会崩

--- merge.py
+++ merge.py
@@ -6,6 +6,34 @@ import time
 import os
 import pandas as pd
 
+
+def saveToAsset(actor , GW , AO):
+    setting = unreal.MeshMergingSettings()
+    setting.pivot_point_at_zero = True
+    setting.merge_materials = False
+
+    options = unreal.EditorScriptingMergeStaticMeshActorsOptions(
+        destroy_source_actors   =   False,
+        spawn_merged_actor      =   False,
+        mesh_merging_settings   =   setting
+    )
+
+    temp_dir = os.path.join('/Game/ARJ_Model',GW,AO)
+
+    actor_name = actor.get_name()
+    actor_label = actor.get_actor_label()
+    asset_path = os.path.join(temp_dir, actor_label).replace('\\','/')
+    print(asset_path)
+    options.base_package_name = asset_path              # The package path you want to save to
+
+    # TODO:将选中的actor合并成 static mesh
+
+    if unreal.EditorAssetLibrary().does_asset_exist(asset_path):
+        print("当前资产 %s 已存在" % actor_label)
+    else:
+        merge_actor = static_mesh_lib.merge_static_mesh_actors([actor],options)     # 单独导出
+        print("EXPORT SUCCESS : %s is save to %s" % (actor.get_actor_label(), asset_path.replace('\\','/')))
+
 # 迭代下去找子级，开销较大
 # def get_all_attached_actors(actor, collected_actors=None):
 #     if collected_actors is None:
@@ -13,7 +41,7 @@ import pandas as pd
     
 #     # Get attached child actors
 #     attached_actors = actor.get_attached_actors()
-    
+
 #     # Add attached actors to the list
 #     for attached_actor in attached_actors:
 #         collected_actors.append(attached_actor)
@@ -27,8 +55,6 @@ static_mesh_lib = unreal.get_editor_subsystem(unreal.StaticMeshEditorSubsystem)
 
 selected_actors = level_lib.get_selected_level_actors()
 selected_actor = level_lib.get_selected_level_actors()[0]
-# print(len(level_lib.get_selected_level_actors()))
-# print(selected_actors[0].get_class().get_name())
 
 all_static_mesh_actors = []
 
@@ -36,56 +62,16 @@ for item in selected_actors:
     if (item.get_name().split('_')[0] == "StaticMeshActor" ):
         all_static_mesh_actors.append(item)
 
-# for i in all_static_mesh_actors:
-#     print(i.get_actor_label())
-#     # 1800个也才9s
-
-# print("111111111111111")
-# 0.0145s
-
 # 检查命名是否匹配
-df = pd.read_csv('C:\\Users\\DELL\\Desktop\\所有工位_0822.csv')
+df = pd.read_csv('C:\\Users\\DELL\\Desktop\\所有工位_0823.csv')
 df_target_col = df.loc[:,['工位','零组件号',"下级工艺件"]]
 
 timeStart = time.time()
 for item in all_static_mesh_actors:
-    print(item.get_actor_label().split('_')[0])
-    df_copy = df[df['零组件号'] == item.get_actor_label().split('_')[0]].copy()
-    # print(df_copy)
-    # for data in df_copy:
-    #     if item.get_actor_label().split('_')[0] == data:
-    #         print(item.get_actor_label().split('_')[0])
-    #         # TODO:save to asset in specific name
-    #         # saveToAsset(item)
-    #         print(f" - {item.get_actor_label()} is save!")
-    #     else:
-    #         print(f" - {item.get_actor_label()} is not find!")
+    df_copy = df_target_col[df['零组件号'] == item.get_actor_label().split('_')[0]].copy()
+    # print(f' %s 找到匹配数据 %d 条' % (item.get_actor_label(),df_copy.size))
+
+    if df_copy.size:
+        for index, row in df_copy.iterrows():
+            saveToAsset(item, str(row['工位']), str(row['下级工艺件'])) 
 print(time.time() - timeStart)
-
-# # selected_static_actors =  unreal.EditorFilterLibrary.by_class(selected_actors , unreal.StaticMeshActor.static_class())
-# # for actor in selected_static_actors:
-# #     actor_class = actor.get_class()
-# #     class_name = actor_class.get_name()
-# #     print(f"Actor: {actor.get_name()}, Class: {class_name}")
-
-# def saveToAsset(actor):
-
-#     setting = unreal.MeshMergingSettings()
-#     setting.pivot_point_at_zero = True
-
-#     options = unreal.EditorScriptingMergeStaticMeshActorsOptions(
-#         destroy_source_actors   =   False,
-#         spawn_merged_actor      =   False,
-#         mesh_merging_settings   =   setting
-#     )
-
-#     temp_dir = '/Game/Temp_Fbx_Export'
-
-#     actor_name = actor.get_name()
-#     asset_path = os.path.join(temp_dir, actor_name)         #在字符中间插入/
-#     options.base_package_name = asset_path
-
-#     # 将选中的actor合并成 static mesh
-#     merge_actor = static_mesh_lib.merge_static_mesh_actors([actor],options)     # 单独导出
-    
-#     print("当前actor保存完毕！")
